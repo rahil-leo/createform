39ecce1ca7b52fd0d71fb3adfd8b39096aca597b
muhammed rahil <rahilleo77@gmail.com>
2025-06-10 18:45:20 +0530

    Delete graphql tests directory

--- graphql tests/graphql-test/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-exports.connectDB = async () => {
-    const MONGO_URI = 'mongodb+srv://jubail:28308000@rahil.f0nsc.mongodb.net/userform';
-    await mongoose.connect(MONGO_URI)
-    console.log('Connected to MongoDB')
-}
--- graphql tests/graphql-test/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    name: String,
-    facebook: String,
-    instagram:String
-});
-
-module.exports = mongoose.model('User', userSchema);
